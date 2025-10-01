25fa70c40d970b9f02a41cf6c99a06da116648ea
Pedro Henrique Harter Marques <pedro.364012@alunosatc.edu.br>
2025-10-01 20:11:07 -0300

    Initial commit

---
+++ assistenciatecnica/src/main/java/com/example/assistenciatecnica/AssistenciatecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.assistenciatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistenciatecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistenciatecnicaApplication.class, args);
+	}
+
+}
---
+++ assistenciatecnica/src/test/java/com/example/assistenciatecnica/AssistenciatecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assistenciatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistenciatecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
